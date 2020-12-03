af811c2560a6f8ca24912f5c3fc3f4ce533a24de
keremcanb <keremcan@gmail.com>
2020-12-03 20:18:56 +0300

    remove api

--- api/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'f486d4a4ba5b68efa564bc92835b9842'),
-    },
-  },
-});
--- api/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '4f793ed0-042a-418a-9e3c-acd39b36e426'
-};

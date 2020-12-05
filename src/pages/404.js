8f1f7b119e154b169456225ffbecf4c4fa384f1b
keremcanb <keremcan@gmail.com>
2020-12-06 02:26:41 +0300

    error comp.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import Layout from '../components/Layout/Layout';
-import SEO from '../components/layout/SEO';
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
 
-const Error = () => <h2>error page</h2>;
+const Error = () => (
+  <Layout>
+    <main className="error-page">
+      <div className="error-container">
+        <h1>Oops it's a dead end...</h1>
+
+        <Link to="/" className="btn">
+          Back Home
+        </Link>
+      </div>
+    </main>
+  </Layout>
+);
 
 export default Error;

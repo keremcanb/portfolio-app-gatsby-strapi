c804b488a1ba25290a92c783c508dd48d11b4db7
keremcanb <keremcan@gmail.com>
2020-12-05 23:53:57 +0300

    footer comp.

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import SocialLinks from '../../constants/socialLinks';
 
-const Footer = () => <h2>footer component</h2>;
+const Footer = () => (
+  <footer className="footer">
+    <div>
+      <SocialLinks styleClass="footer-links" />
+      <h4>
+        copyright &copy; {new Date().getFullYear()} <span>WebDev</span> All Rights Reserved
+      </h4>
+    </div>
+  </footer>
+);
 
 export default Footer;

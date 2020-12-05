9b141d4f1aa45a3ccd4e88d11747fab99672998c
keremcanb <keremcan@gmail.com>
2020-12-05 22:19:46 +0300

    blog template

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -3,15 +3,32 @@ import { graphql, Link } from 'gatsby';
 import ReactMarkdown from 'react-markdown';
 import Layout from '../components/Layout/Layout';
 
-const ComponentName = () => <h2>blog post template</h2>;
+const ComponentName = ({
+  data: {
+    blog: { content },
+  },
+}) => (
+  <Layout>
+    <section className="blog-template">
+      <div className="section-center">
+        <article className="blog-content">
+          <ReactMarkdown source={content} />
+        </article>
 
-// export const query = graphql`
-//   query GetSingleBlog($slug: String) {
-//     blog: strapiBlogs(slug: { eq: $slug }) {
-//       content
+        <Link to="/blog" className="btn center-btn">
+          Blog
+        </Link>
+      </div>
+    </section>
+  </Layout>
+);
 
-//     }
-//   }
-// `
+export const query = graphql`
+  query GetSingleBlog($slug: String) {
+    blog: strapiBlogs(slug: { eq: $slug }) {
+      content
+    }
+  }
+`;
 
 export default ComponentName;
